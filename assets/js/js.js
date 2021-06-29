ec377540512b863dc3ed397bcf3c183af11ab8da
ChunYu Chang <lulutofulucas@ChunYus-MacBook-Air.local>
2021-06-29 11:41:44 -0400

    draft

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,1 +1,47 @@
 console.log("js.js linked");
+
+var closeW = document.getElementById("noBtn");
+
+
+
+closeW.addEventListener("click", function(e){
+    e.preventDefault();
+    countdown_init();
+
+});
+
+
+
+var countdown;
+var countdown_number;
+   
+    function countdown_init() {
+        countdown_number = 6;
+        countdown_trigger();
+    }
+   
+    function countdown_trigger() {
+        if (countdown_number > 0) {
+            countdown_number--;
+   
+            document.getElementById('countdown').innerHTML = "Time: " + countdown_number;
+   
+            if(countdown_number > 0) {
+                countdown = setTimeout('countdown_trigger()', 1000);
+            }else{
+                window.location.href = "thanks.html";
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
